f61ed56bad359a8176fa8c18f28940c4008b886c
hunterD3CD <hunterd3c@gmail.com>
2021-07-04 15:37:01 -0700

    npm init -y, npm install inquirer

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// TODO: Include packages needed for this application
+//---------------------------------Include packages needed for this application-------------------------------------
+// fs module to write new file; inquirer module to ask user questio; generateMarkdown is customized module;
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
