4164f8724fe36b95ab21dc7914807a3a00523c62
hunterD3CD <hunterd3c@gmail.com>
2021-07-04 20:40:35 -0700

    link the table of content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,13 +18,13 @@ function generateMarkdown(data) {
   [![license](https://img.shields.io/badge/license-${data.license}-blue)](https://shields.io)
 
   ## Table of Contents:
-    1. [Description](#description)
-    2. [Installation](#installation)
-    3. [Usage](#usage)
-    4. [Credits](#credits)
-    5. [License](#license)
-    6. [Contributing](#contributing)
-    7. [Tests](#tests)
+    1. [Description](https://github.com/hunterD3CD/09-READ.ME-Generator#description)
+    2. [Installation](https://github.com/hunterD3CD/09-READ.ME-Generator#installation)
+    3. [Usage](https://github.com/hunterD3CD/09-READ.ME-Generator#usage)
+    4. [Credits](https://github.com/hunterD3CD/09-READ.ME-Generator#credits)
+    5. [License](https://github.com/hunterD3CD/09-READ.ME-Generator#license)
+    6. [Contributing](https://github.com/hunterD3CD/09-READ.ME-Generator#contributing)
+    7. [Tests](https://github.com/hunterD3CD/09-READ.ME-Generator#tests)
     
   ## Description:
   ${data.description}
